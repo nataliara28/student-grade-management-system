d679ba1ba54e55d12a091e1b06eb36c4b4117dab
nataliara28 <natalia@faculty.masterschool.com>
2024-10-02 12:17:48 +0200

    Added a function to classify the students that passed or failed

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def calculate_average(students):
     averages = {}
     for student, grades in students.items():
         averages[student] = sum(grades) / len(grades)
+    
     return averages
 
 
 # 3. Function to find the top student based on average grades
-
 def find_top_student(averages):
     top_student = None
     top_avg = 0
@@ -31,13 +31,24 @@ def find_top_student(averages):
         if avg > top_avg:
             top_avg = avg
             top_student = student
+    
     return top_student, top_avg
-    
-    
-    
-    
 
+# 4. Function to classify students as passed or failed / if average is 60 
 
+def classify_students(averages, passing_mark):
+    passed_students = []
+    failed_students = []
+    
+    for student, avg in averages.items():
+        if avg >= passing_mark:
+            passed_students.append(student)
+        else:
+            failed_students.append(student)
+    
+    return passed_students, failed_students
+    
+    
 def main():
     num_students = 5
     num_grades = 3
@@ -59,6 +70,19 @@ def main():
     # 3. Find the top student
     top_student, top_avg = find_top_student(averages)
     print(f"\nTop Student is: {top_student} with an average of {top_avg:.2f}")
+    
+    # 4. Classify students who passed or failed
+    passed_students, failed_students = classify_students(averages, 60)
+    
+    if passed_students:
+        print('\nPassed Students:')
+        for student in passed_students:
+            print(student)
+    
+    if failed_students:
+        print('\nFailed Students:')
+        for student in failed_students:
+            print(student)
 
 if __name__ == "__main__":
     main()
